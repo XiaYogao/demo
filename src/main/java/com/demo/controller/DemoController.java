6108d3f41e8fc6f88de47fa43f7899301dc5eec1
gus <sharygus@gmail.com>
2016-09-06 15:15:26 +0800

    add spring configuration files.

--- src/main/java/com/demo/controller/DemoController.java
+++ src/main/java/com/demo/controller/DemoController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * TODO 功能描述
+ * 用户控制器
  *
  * @author sharygus@gmail.com
  * @date 2016/9/6 14:39
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DemoController {
 
     @RequestMapping("/")
-    public String index(){
+    public String index() {
         return "index";
     }
 }
---
+++ src/main/java/com/demo/service/IUserService.java
@@ -0,0 +1,22 @@
+package com.demo.service;
+
+import com.demo.model.User;
+
+import java.util.List;
+
+/**
+ * User service interface class
+ *
+ * @author sharygus@gmail.com
+ * @date 2016/9/6 15:00
+ * @since V1.0
+ */
+public interface IUserService {
+
+    int saveOrUpdate(User user);
+
+    int delete(Long id);
+
+    List<User> list(User user);
+
+}
